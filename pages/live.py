35b958a78b5b5ae0922dc43e1138afdccb8f2b90
Aditya Rao <82911340+adityarao2005@users.noreply.github.com>
2025-01-11 20:45:19 +0000

    sent another requirements txt

--- pages/live.py
+++ pages/live.py
@@ -1,1 +1,19 @@
 # Facial recognition setup
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer
+import av
+st.title("Live feed")
+
+# Placeholder for live feed functionality
+st.write("This is where the live feed will be displayed.")
+
+
+def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
+    img = frame.to_ndarray(format="bgr24")
+
+    flipped = img[::-1,:,:]
+
+    return av.VideoFrame.from_ndarray(flipped, format="bgr24")
+
+
+webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
