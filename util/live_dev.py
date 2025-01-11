343b3753bc2777ab1347a31006326e13aa7381af
Andy Pak <im.andy.pak@gmail.com>
2025-01-11 17:46:59 -0500

    Fix camera feed

--- util/live_dev.py
+++ util/live_dev.py
@@ -1,18 +1,23 @@
 # Facial recognition setup
 import streamlit as st
 import cv2
+import numpy as np
 
 def main():
-    cam = cv2.VideoCapture(0)
-    cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    st.title("Facial Recognition App")
     FRAME_WINDOW = st.image([])
 
     while True:
-        ret, frame = cam.read()
-        if not ret:
-            st.error("Failed to capture frame from camera")
-            st.info("Please turn off the other app that is using the camera and restart app")
-            st.stop()
-        
-        FRAME_WINDOW.image(frame)
+        img_file_buffer = st.camera_input("Capture an image")
+
+        if img_file_buffer is not None:
+            # Convert the image to a format for cv2
+            bytes_data = img_file_buffer.getvalue()
+            np_array = np.frombuffer(bytes_data, np.uint8)
+            frame = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+
+            # make grey
+            gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            # Display the processed frame
+            FRAME_WINDOW.image(gray_frame, channels="GRAY")
